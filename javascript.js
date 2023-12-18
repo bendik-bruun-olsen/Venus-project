1f8378c9367ee14e6383b7bd85ddda9ea8bc3cb7
Bendik Kodehode <bendik08.kodehode@gmail.com>
2023-12-18 22:16:58 +0100

    added comments to js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   slideshow(1);
-};
+}
 
 var slidePos = 1;
 slideshow(slidePos);
@@ -18,21 +18,24 @@ function slideshow(n) {
   var slides = document.getElementsByClassName("slideshow-item");
   var circles = document.getElementsByClassName("dots");
 
+  // Switches back to first if end reached
   if (n > slides.length) {
     slidePos = 1;
   }
 
+  // Switches to last if position 0 reached
   if (n < 1) {
     slidePos = slides.length;
   }
-
+  // Disable all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
+  // Disable all dots
   for (i = 0; i < circles.length; i++) {
     circles[i].className = circles[i].className.replace(" enable", "");
   }
+  // Initialize slides and dots
   slides[slidePos - 1].style.display = "block";
   circles[slidePos - 1].className += " enable";
 }
